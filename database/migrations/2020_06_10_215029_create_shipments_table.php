a539ba505bc5e44c461bd7eadb09f7685bd6e7f4
Abir <rahmanmushfiq94@gmail.com>
2020-06-11 03:53:11 +0600

    shipments table added

---
+++ database/migrations/2020_06_10_215029_create_shipments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShipmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('name', 1000);
+            $table->integer('price')->unsigned()->default(0);
+            $table->integer('rating')->unsigned()->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipments');
+    }
+}
